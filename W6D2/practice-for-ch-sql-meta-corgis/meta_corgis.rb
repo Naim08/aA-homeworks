4b9a0a94db416f2a3fa4dcfe0b8ecf796374a9fd
Naim Miah <mmiah@fordham.edu>
2023-08-07 00:50:16 -0400

    compleeted most of hw

--- W6D2/practice-for-ch-sql-meta-corgis/meta_corgis.rb
+++ W6D2/practice-for-ch-sql-meta-corgis/meta_corgis.rb
@@ -1,48 +1,52 @@
+# frozen_string_literal: true
+
+# Represents a collection of snacks for corgis.
 class SnackBox
   SNACK_BOX_DATA = {
     1 => {
-      "bone" => {
-        "info" => "Phoenician rawhide",
-        "tastiness" => 20
+      'bone' => {
+        'info' => "Phoenician rawhide",
+        "tastiness" => 20,
       },
-      "kibble" => {
+      'kibble' => {
         "info" => "Delicately braised hamhocks",
-        "tastiness" => 33
+        'tastiness' => 33,
       },
       "treat" => {
         "info" => "Chewy dental sticks",
-        "tastiness" => 40
-      }
+        "tastiness" => 40,
+      },
     },
     2 => {
       "bone" => {
         "info" => "An old dirty bone",
-        "tastiness" => 2
+        "tastiness" => 2,
       },
       "kibble" => {
         "info" => "Kale clusters",
-        "tastiness" => 1
+        "tastiness" => 1,
       },
       "treat" => {
         "info" => "Bacon",
-        "tastiness" => 80
-      }
+        "tastiness" => 80,
+      },
     },
     3 => {
       "bone" => {
         "info" => "A steak bone",
-        "tastiness" => 64
+        "tastiness" => 64,
       },
       "kibble" => {
         "info" => "Sweet Potato nibbles",
-        "tastiness" => 45
+        "tastiness" => 45,
       },
       "treat" => {
         "info" => "Chicken bits",
-        "tastiness" => 75
-      }
+        "tastiness" => 75,
+      },
     }
-  }
+  }.freeze
+
   def initialize(data = SNACK_BOX_DATA)
     @data = data
   end
@@ -72,8 +76,8 @@ class SnackBox
   end
 end
 
+# Represents a collection of snacks for corgis.
 class CorgiSnacks
-
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
@@ -99,10 +103,13 @@ class CorgiSnacks
     result = "Treat: #{info}: #{tastiness} "
     tastiness > 30 ? "* #{result}" : result
   end
-
 end
 
+# Represents a meta corgi snack box
 class MetaCorgiSnacks
+  # BEGIN: ed8c6549bwf9
+
+  # END: ed8c6549bwf9
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
@@ -121,13 +128,18 @@ class MetaCorgiSnacks
   # end
   def method_missing(name, *args)
     method_name = name.to_s
-    if %w(bone kibble treat).include?(method_name)
+    if %w[bone kibble treat].include?(method_name)
       self.class.define_snack(method_name)
       send(method_name)
     else
       super
     end
   end
+
+  def respond_to_missing?(method_name, include_private = false)
+    %w[bone kibble treat].include?(method_name.to_s) || super
+  end
+
   def self.define_snack(name)
     define_method(name) do
       method_name = name.to_s
@@ -144,3 +156,15 @@ meta_snacks = MetaCorgiSnacks.new(snack_box, 1)
 p meta_snacks.bone
 p meta_snacks.kibble
 p meta_snacks.treat
+
+class Corgi
+  def self.bark(sound)
+    define_method(sound) { puts sound}
+  end
+
+  bark(:hello)
+  bark(:world)
+end
+
+c = Corgi.new
+p.bark
